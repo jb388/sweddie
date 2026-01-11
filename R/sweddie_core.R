5363442a4475bec3cc8907a97cf7a815dcb1dee0
jb388 <jbeemmil@gmail.com>
2026-01-11 12:42:13 +0100

    add documentation for data files

---
+++ R/sweddie_core.R
@@ -0,0 +1,9 @@
+#' SWEDDIE core data
+#'
+#' @description Core data tables for SWEDDIE
+#' @details List of experiments in SWEDDIE and their associated experiment, site, and plot data tables.
+#' @usage data(sweddie_core)
+#'
+#' @format list
+
+"sweddie_core"
---
+++ R/sweddie_meta.R
@@ -0,0 +1,9 @@
+#' SWEDDIE metadata
+#'
+#' @description Metadata tables for SWEDDIE
+#' @details List of experiments in SWEDDIE and their associated metadata: file level metadata (flmd), and data dictionary files (dd)
+#' @usage data(sweddie_meta)
+#'
+#' @format list
+
+"sweddie_meta"
