0db795835910173e81d7e6f6fef38b18fbc8e482
jb388 <jbeemmil@gmail.com>
2026-01-14 22:55:17 +0100

    adapt read_csv_cmp to accept .csv or .csv.gz input

--- R/read_csv_cmp.R
+++ R/read_csv_cmp.R
@@ -7,6 +7,21 @@
 #' @importFrom utils read.csv
 #' @export
 read_csv_cmp <- function(path, ...) {
+
+  # If path has no extension, try .csv then .csv.gz
+  if (!grepl("\\.(csv|csv\\.gz)$", path, ignore.case = TRUE)) {
+    path_csv <- paste0(path, ".csv")
+    path_csv_gz <- paste0(path, ".csv.gz")
+
+    if (file.exists(path_csv)) {
+      path <- path_csv
+    } else if (file.exists(path_csv_gz)) {
+      path <- path_csv_gz
+    } else {
+      stop("File not found: ", path, "(.csv or .csv.gz)")
+    }
+  }
+
   if (grepl("\\.gz$", path, ignore.case = TRUE)) {
     con <- gzfile(path, open = "rt")
     on.exit(close(con), add = TRUE)
