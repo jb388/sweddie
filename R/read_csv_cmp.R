bf90eeb568e588e991eefa2aaa4ff1f5398aedf3
jb388 <jbeemmil@gmail.com>
2026-02-09 13:28:22 +0100

    fixed encoding issue in read_csv_cmp

--- R/read_csv_cmp.R
+++ R/read_csv_cmp.R
@@ -5,28 +5,49 @@
 #' @return file
 #' @details runs 'read.csv' and decompresses GZ files as needed
 #' @importFrom utils read.csv
+#' @importFrom readr guess_encoding
 #' @export
 read_csv_cmp <- function(path, ...) {
 
-  # If path has no extension, try .csv then .csv.gz
+  # resolve extension
   if (!grepl("\\.(csv|csv\\.gz)$", path, ignore.case = TRUE)) {
-    path_csv <- paste0(path, ".csv")
-    path_csv_gz <- paste0(path, ".csv.gz")
-
-    if (file.exists(path_csv)) {
-      path <- path_csv
-    } else if (file.exists(path_csv_gz)) {
-      path <- path_csv_gz
+    if (file.exists(paste0(path, ".csv"))) {
+      path <- paste0(path, ".csv")
+    } else if (file.exists(paste0(path, ".csv.gz"))) {
+      path <- paste0(path, ".csv.gz")
     } else {
-      stop("File not found: ", path, "(.csv or .csv.gz)")
+      stop("File not found: ", path)
     }
   }
 
-  if (grepl("\\.gz$", path, ignore.case = TRUE)) {
-    con <- gzfile(path, open = "rt")
-    on.exit(close(con), add = TRUE)
-    read.csv(con, fileEncoding = "UTF-8", ...)
-  } else {
-    read.csv(path, fileEncoding = "UTF-8", ...)
+  # guess encoding
+  enc_guess <- tryCatch(
+    readr::guess_encoding(path, n_max = 10000)$encoding[1],
+    error = function(e) "UTF-8"
+  )
+
+  # get ...
+  base_args <- list(...)
+
+  read_attempt <- function(enc) {
+    args <- c(base_args, list(fileEncoding = enc))
+
+    if (grepl("\\.gz$", path, ignore.case = TRUE)) {
+      con <- gzfile(path, open = "rt")
+      on.exit(close(con), add = TRUE)
+      args$file <- con
+    } else {
+      args$file <- path
+    }
+
+    do.call(read.csv, args)
   }
+
+  tryCatch(
+    read_attempt(enc_guess),
+    error = function(e) {
+      message("Retrying with Windows-1252 encoding")
+      read_attempt("Windows-1252")
+    }
+  )
 }
